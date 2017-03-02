245e7577247265a3c4bb3a613da4214b35d53007
sruthykjoseph <sruthyjoseph100@gmail.com>
2017-03-03 00:05:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,15 +95,8 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res) {
     //articleName = article-one
     //articles[articleName] == {} content object for article one
+    var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
-});
-
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/article-four', function (req, res) {
