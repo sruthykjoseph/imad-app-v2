b54f14b8faae9fe599568ab6ae6db0d4b5b35fcd
sruthykjoseph <sruthyjoseph100@gmail.com>
2017-03-20 21:01:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,8 +31,7 @@ button.onclick = function(){
   
 };
 //Submit name
-var  nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     // Make a request to the server and send the name
@@ -63,6 +62,8 @@ submit.onclick = function () {
   };
   
   //Make the request
+  var  nameInput = document.getElementById('name');
+  var name = nameInput.value;
   request.open('GET', 'http://sruthykjoseph.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);
 };
