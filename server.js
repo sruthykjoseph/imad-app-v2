7fe29a924e394fd8dee07fe947bab36bf71b7a8f
sruthykjoseph <sruthyjoseph100@gmail.com>
2017-03-20 20:37:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,6 +98,19 @@ app.get('/counter', function (req, res) {
    res.send(counter.toString());
 });
 
+var names = [];
+//app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name', function (req, res) { // URL: /submit-name?name=xxxx
+    //Get the name from the request
+    //var name = req.params.name;
+    var name = req.query.name;
+    
+    names.push(name);
+    //JSON : JavaScript Object Notation
+    res.send(JSON.stringify(names)); //TODO
+    
+});
+
 app.get('/:articleName', function (req, res) {
     //articleName = article-one
     //articles[articleName] == {} content object for article one
@@ -121,18 +134,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names = [];
-//app.get('/submit-name/:name', function (req, res) {
-app.get('/submit-name', function (req, res) { // URL: /submit-name?name=xxxx
-    //Get the name from the request
-    //var name = req.params.name;
-    var name = req.query.name;
-    
-    names.push(name);
-    //JSON : JavaScript Object Notation
-    res.send(JSON.stringify(names)); //TODO
-    
-});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
