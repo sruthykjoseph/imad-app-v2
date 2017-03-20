eeca9ba4258592a08e14a2c4c1f6051c49e15315
sruthykjoseph <sruthyjoseph100@gmail.com>
2017-03-20 14:10:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,6 @@ submit.onclick = function () {
     for (var i = 0; i< names.length; i++) {
         list+= '<li>' + names[i] + '</li>';
     }
-    
     var ul = document.getElementById('namelist');
     ul.inner.HTML = list;
 };
