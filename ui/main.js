1dcb0c27dd57c59e6356615f3e8c8a03f4a8f3be
sruthykjoseph <sruthyjoseph100@gmail.com>
2017-03-20 13:52:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,8 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     // Make a request to the server and send the name
-    // Caputure the list of names and render it as a list
+    
+    // Caputure a list of names and render it as a list
     var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
     for (var i = 0; i< names.length; i++) {
