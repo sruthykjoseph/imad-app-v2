db28df4534265d300fe2275a0f902f5295f8890e
sruthykjoseph <sruthyjoseph100@gmail.com>
2017-03-20 21:00:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@
 var button = document.getElementById('counter');
 
 button.onclick = function(){
+    
   // Create the request object
   var request = new XMLHttpRequest();
   
@@ -10,9 +11,14 @@ button.onclick = function(){
      if (request.readyState === XMLHttpRequest.DONE) {
          //Take an action
          if (request.status === 200) {
-             var counter = request.responseText;
-             var span = document.getElementById('count');
-             span.innerHTML = counter.toString();
+             // Caputure a list of names and render it as a list
+            var names = ['name1', 'name2', 'name3', 'name4'];
+            var list = '';
+            for (var i=0; i< names.length; i++) {
+                list += '<li>' + names[i] + '</li>';
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
          }
      }
     
@@ -31,12 +37,32 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     // Make a request to the server and send the name
     
-    // Caputure a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for (var i=0; i< names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    // Create the request object
+  var request = new XMLHttpRequest();
+  
+  // Capture the response and store it in a variable
+  request.onreadystatechange = function () {
+     if (request.readyState === XMLHttpRequest.DONE) {
+         //Take an action
+         if (request.status === 200) {
+             // Caputure a list of names and render it as a list
+            //var names = ['name1', 'name2', 'name3', 'name4'];
+            var names = request.responseText;
+            names = JSON.parse(names);
+
+            var list = '';
+            for (var i=0; i< names.length; i++) {
+                list += '<li>' + names[i] + '</li>';
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+         }
+     }
+    
+    // Not done yet
+  };
+  
+  //Make the request
+  request.open('GET', 'http://sruthykjoseph.imad.hasura-app.io/submit-name?name=' + name, true);
+  request.send(null);
 };
