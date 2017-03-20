7725159264edf33445b673931cd2b16ce321607d
sruthykjoseph <sruthyjoseph100@gmail.com>
2017-03-20 21:09:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,7 +48,6 @@ submit.onclick = function () {
             //var names = ['name1', 'name2', 'name3', 'name4'];
             var names = request.responseText;
             names = JSON.parse(names);
-
             var list = '';
             for (var i=0; i< names.length; i++) {
                 list += '<li>' + names[i] + '</li>';
